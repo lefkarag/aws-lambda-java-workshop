d6547cef2ed8d0b8b54322f9240eb4729266bb6e
Lefteris Karageorgiou <lefkarag@amazon.com>
2025-04-03 11:25:58 +0300

    added script logs

--- infrastructure/cdk/src/main/java/com/unicorn/UnicornStoreInfraStack.java
+++ infrastructure/cdk/src/main/java/com/unicorn/UnicornStoreInfraStack.java
@@ -19,10 +19,10 @@ public class UnicornStoreInfraStack extends Stack {
             sudo -H -u ec2-user bash -c "cd ~/environment/aws-lambda-java-workshop && git checkout cdk-refactoring-split"
             
             echo '=== Setup IDE ==='
-            sudo -H -i -u ec2-user bash -c "~/environment/aws-lambda-java-workshop/infrastructure/scripts/setup-ide.sh"
+            sudo -H -i -u ec2-user bash -c "~/environment/aws-lambda-java-workshop/infrastructure/scripts/setup-ide.sh >> /home/ec2-user/setup-ide.log"
             
             echo '=== Additional Setup ==='
-            sudo -H -i -u ec2-user bash -c "~/environment/aws-lambda-java-workshop/labs/unicorn-store/setup-infrastructure.sh"
+            sudo -H -i -u ec2-user bash -c "~/environment/aws-lambda-java-workshop/labs/unicorn-store/setup-infrastructure.sh >> /home/ec2-user/setup-infra.log"
             """;
 
     public UnicornStoreInfraStack(final Construct scope, final String id, final StackProps props) {
